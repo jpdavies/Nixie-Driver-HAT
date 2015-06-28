cba445d0c2693ec7544c0673087a4d11ccbda8b3
jpdavies <jon.davies@gmail.com>
2015-06-28 01:27:23 +0100

    Update compile instructions
    Added mandatory -lwiringPi argument.

--- Source Code/blink/blink.c
+++ Source Code/blink/blink.c
@@ -1,9 +1,11 @@
 /*
 Compile instructions:
 [e.g.]
-gcc -o binaryFile sourceFile1.c [sourceFile2.c sourceFileNNN.c]
+gcc -o binaryFile sourceFile1.c [sourceFile2.c sourceFileNNN.c] -lwiriingPi
 [actual]
-gcc -o blink blink.c
+gcc -o blink blink.c -lwiriingPi
+Needed arguments:
+-lwiriingPi: Link 'Wiring for Pi' library
 */
 #include <wiringPi.h>
 int main (void)
